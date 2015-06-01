ef53eaf3d5b550468e7acdbceb1e757caee8305a
Velsu <riftmage50@gmail.com>
2015-06-01 12:58:59 +0200

    Configure controller, create views for jobs, add a form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,8 @@
 class JobsController < ApplicationController
+	before_action :set_job, except: [:new, :create, :index]
 
 	def index
+		@jobs = Job.all.order(created_at: :desc)
 	end
 
 	def show
@@ -10,14 +12,36 @@ class JobsController < ApplicationController
 	end
 
 	def new
+		@job = Job.new
 	end
 
 	def create
+		@job = Job.create(job_params)
+
+		if @job.save
+			redirect_to @job, notice: "Saved successfully"
+		else
+			render 'new'
+		end
 	end
 
 	def update
 	end
 
 	def destroy
+		if @job.destroy
+			redirect_to root_url, notice: "Job deleted"
+		end
+	end
+
+
+	private
+
+	def set_job
+		@job = Job.find(params[:id])
+	end
+
+	def job_params
+		params.require(:job).permit(:title, :description, :company, :url)
 	end
 end
